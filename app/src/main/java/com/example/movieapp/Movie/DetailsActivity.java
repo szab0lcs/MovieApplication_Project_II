55334195932ef9b249b0542a9e04820daeb3af17
szab0lcs <szabolcskencse@gmail.com>
2020-01-07 17:32:04 +0100

    Create Details screen

---
+++ app/src/main/java/com/example/movieapp/Movie/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.movieapp.Movie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.movieapp.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}

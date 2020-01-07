2c8dccb3bfd77304c2c109dd3b20081cef941e03
szab0lcs <szabolcskencse@gmail.com>
2020-01-07 20:05:03 +0100

    Create Favorite and Profile Activity

---
+++ app/src/main/java/com/example/movieapp/ProfileAndFavoriteActivity/ProfileActivity.java
@@ -0,0 +1,77 @@
+package com.example.movieapp.ProfileAndFavoriteActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.movieapp.Database.DatabaseHelper;
+import com.example.movieapp.Main.MainActivity;
+import com.example.movieapp.R;
+import com.example.movieapp.User.User;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private DatabaseHelper databaseHelper;
+    private TextView nameFiled, emailField, ageField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        init();
+
+        bottomNavigation();
+        
+        ListData();
+
+    }
+
+    private void bottomNavigation() {
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.nav_home:
+                        Intent intent1 = new Intent(ProfileActivity.this, MainActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.nav_fav:
+                        Intent intent2 = new Intent(ProfileActivity.this, FavoriteActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.nav_profile:
+                        Intent intent3 = new Intent(ProfileActivity.this, ProfileActivity.class);
+                        startActivity(intent3);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void init() {
+        nameFiled = findViewById(R.id.pTextViewNameField);
+        ageField = findViewById(R.id.pTextViewAgeInputField);
+        emailField = findViewById(R.id.pTextViewEmailInputField);
+    }
+
+    private void ListData() {
+        Intent intent = getIntent();
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+        User user = databaseHelper.profileLogin(intent.getStringExtra("email"));
+        Log.d("alma", user.getName());
+        nameFiled.setText(user.getName());
+        ageField.setText(user.getAge());
+        emailField.setText(user.getEmail());
+    }
+}
